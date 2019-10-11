3d9ae8afc3538022f0d9c47ea43940b5e486388b
fhyPayaso <410619823@qq.com>
2019-10-11 17:56:01 +0800

    【Catmull_Clark】

--- Subdivision/MeshSubdivision.cpp
+++ Subdivision/MeshSubdivision.cpp
@@ -51,7 +51,7 @@ Mesh3D *MeshSubdivision::Doo_Sabin()
 		HE_edge* temp_edge = (*fiter)->edge;
 		// 遍历当前面的所有边
 		VERTEX_LIST*  v_list = new VERTEX_LIST;
-        while (temp_edge != anchor_edge);
+        do
 		{
 			float new_x = 0, new_y = 0, new_z = 0;
 			// 当前边所指向的顶点
@@ -60,7 +60,7 @@ Mesh3D *MeshSubdivision::Doo_Sabin()
 			HE_edge* alpha_edge = temp_edge;
 
 			int i = 0;
-			while (alpha_edge != temp_edge)
+			do
             {
                 alpha = (i == 0 ? n + 5 : 3 + 2 * std::cos(2 * PI * i / n)) / (4 * n * 1.0);
 
@@ -72,7 +72,7 @@ Mesh3D *MeshSubdivision::Doo_Sabin()
                 // 迭代计算下一个顶点相对新顶点的权重
                 alpha_edge = alpha_edge->next;
                 cur_edge_vert = alpha_edge->vert;
-            }
+            }while (alpha_edge != temp_edge)
 
 			// 将计算出的新顶点插入新网格
 			HE_vert* new_vert = New_mesh->insert_vertex(new_x, new_y, new_z);
@@ -83,7 +83,7 @@ Mesh3D *MeshSubdivision::Doo_Sabin()
 			v_list->push_back(new_vert);
 
 			temp_edge = temp_edge->next;
-		}
+		}while (temp_edge != anchor_edge);
 		// 插入当前面所对应的F-faces
 		New_mesh->insert_face(*v_list);
 		delete v_list;
@@ -166,4 +166,142 @@ Mesh3D *MeshSubdivision::Catmull_Clark()
 	{
 		return NULL;
 	}
+
+    Mesh3D *New_mesh = new Mesh3D;
+	// 初始化id映射数组
+    std::vector<int> f_vert_id_list ,e_vert_id_list ,v_vert_id_list;
+    f_vert_id_list.assign(m_pmesh->get_num_of_faces_list(),0);
+    e_vert_id_list.assign(m_pmesh->get_num_of_edges_list(),0);
+    v_vert_id_list.assign(m_pmesh->get_num_of_vertices_list(),0);
+	//==================================================
+	// Step 1 : 对于旧网格每个面，生成新的面顶点，并插入新网格
+	//==================================================
+    PTR_FACE_LIST face_list = m_pmesh->get_faces_list();
+    for (FACE_ITER fiter = face_list->begin(); fiter != face_list->end(); fiter++)
+    {
+        float new_x = 0, new_y = 0, new_z = 0;
+        HE_edge* firstEdge = (*fiter)->edge;
+        HE_edge* curEdge = firstEdge;
+        int n = (*fiter)->valence;
+        do
+        {
+            HE_vert* curVert = curEdge->vert;
+            new_x += curVert->x * 1.0 / n;
+            new_y += curVert->y * 1.0 / n;
+            new_z += curVert->z * 1.0 / n;
+
+            curEdge = curEdge->next;
+        }while (firstEdge != curEdge);
+        // 将计算出的面顶点插入新网格
+        HE_vert* new_vert = New_mesh->insert_vertex(new_x, new_y, new_z);
+        // 存储面与新面顶点之间的id对应关系
+        f_vert_id_list[(*fiter)->id] = new_vert->id;
+    }
+
+    //==================================================
+    // Step 2 : 对于旧网格每个边，生成新的边顶点，并插入新网格
+    //==================================================
+    PTR_EDGE_LIST edge_list = m_pmesh->get_edges_list();
+    // 初始化标记位
+    for(EDGE_ITER eiter = edge_list->begin(); eiter != edge_list->end() ; eiter++)
+    {
+        (*eiter)->tag = true;
+    }
+    for(EDGE_ITER eiter = edge_list->begin(); eiter != edge_list->end() ; eiter++)
+    {
+        if((*eiter)->tag)
+        {
+            float new_x = 0, new_y = 0, new_z = 0;
+            HE_edge* curEdge = *eiter;
+            HE_edge* pairEdge = curEdge->pair;
+            // 先获取旧网格的顶点
+            HE_vert* left_vert = curEdge->vert;
+            HE_vert* right_vert = pairEdge->vert;
+            // 获取新的面顶点
+            HE_vert* up_vert =  New_mesh->get_vertex(f_vert_id_list[curEdge->face->id]);
+            HE_vert* down_vert =  New_mesh->get_vertex(f_vert_id_list[pairEdge->face->id]);
+            // 计算新顶点
+            new_x = (left_vert->x + right_vert->x + up_vert->x + down_vert->x)/4;
+            new_y = (left_vert->y + right_vert->y + up_vert->y + down_vert->y)/4;
+            new_z = (left_vert->z + right_vert->z + up_vert->z + down_vert->z)/4;
+            // 将计算出的边顶点插入新网格
+            HE_vert* new_vert = New_mesh->insert_vertex(new_x, new_y, new_z);
+            // 存储边与新边顶点之间的id对应关系
+            e_vert_id_list[curEdge->id] = new_vert->id;
+            e_vert_id_list[pairEdge->id] = new_vert->id;
+            // 两个半边只生成一个边顶点
+            curEdge->tag = false;
+            pairEdge->tag = false;
+        }
+    }
+    //===================================================
+    // Step 3 : 对于旧网格每个顶点，生成新的顶点，并插入新网格
+    //          对应公式 => (Q + 2R + (n−3)S) / n
+    //===================================================
+    PTR_VERTEX_LIST vert_list = m_pmesh->get_vertices_list();
+    for(VERTEX_ITER viter = vert_list->begin() ;viter != vert_list->end() ;viter++)
+    {
+        // Q是该顶点相邻的面的新顶点的平均
+        HE_vert* Q = new HE_vert(0.0,0.0,0.0);
+        // R是该顶点的相邻边的中点的平均
+        HE_vert* R = new HE_vert(0.0,0.0,0.0);
+        // S为原顶点
+        HE_vert* S = *viter;
+        //n是该顶点相邻的边的数目
+        int n = S->degree;
+        // 构造Q和R
+        HE_edge* firstEdge = S->edge;
+        HE_edge* curEdge = S->edge;
+        do
+        {
+            // 获取新的面顶点和边顶点
+            HE_vert* f_vert = New_mesh->get_vertex(f_vert_id_list[curEdge->face->id]);
+            HE_vert* e_vert = New_mesh->get_vertex(e_vert_id_list[curEdge->id]);
+            // 累加坐标值
+            Q->x += f_vert->x; Q->y += f_vert->y; Q->z += f_vert->z;
+            R->x += e_vert->x; R->y += e_vert->y; R->z += e_vert->z;
+            curEdge = curEdge->pair->next;
+        }while (curEdge != firstEdge);
+        // 计算新顶点
+        Q->x /= n; Q->y /= n; Q->z /= n;
+        R->x /= n; R->y /= n; R->z /= n;
+        float new_x = 0, new_y = 0, new_z = 0;
+        new_x = (Q->x + 2 * R->x + (n-3) * S->x) / n;
+        new_y = (Q->y + 2 * R->y + (n-3) * S->y) / n;
+        new_z = (Q->z + 2 * R->z + (n-3) * S->z) / n;
+        // 插入新顶点
+        HE_vert* new_vert = New_mesh->insert_vertex(new_x, new_y, new_z);
+        // 存储旧顶点和新顶点之间的id对应关系
+        v_vert_id_list[S->id] = new_vert->id;
+    }
+    //===================================================
+    // Step 4 : 开始构造面
+    //          (1)一个新的面顶点和与包围该面的新的边顶点连接
+    //          (2)一个新的顶点顶点和与该顶点相邻的新的边顶点连接
+    //===================================================
+    for (FACE_ITER fiter = face_list->begin(); fiter != face_list->end(); fiter++)
+    {
+        HE_vert* f_vert = New_mesh->get_vertex(f_vert_id_list[(*fiter)->id]);
+        HE_edge* firstEdge = (*fiter)->edge;
+        HE_edge* curEdge = (*fiter)->edge;
+        VERTEX_LIST*  v_list = new VERTEX_LIST;
+        do
+        {
+            // 构造并插入新的面
+            v_list->push_back(New_mesh->get_vertex(e_vert_id_list[curEdge->id]));
+            v_list->push_back(New_mesh->get_vertex(v_vert_id_list[curEdge->vert->id]));
+            v_list->push_back(New_mesh->get_vertex(e_vert_id_list[curEdge->next->id]));
+            v_list->push_back(f_vert);
+            New_mesh->insert_face(*v_list);
+            v_list->clear();
+
+            curEdge = curEdge->next;
+        }while (firstEdge != curEdge);
+    }
+    /////////////////////////////////////////////////
+    New_mesh->update_mesh();
+
+    delete m_pmesh;
+    m_pmesh = NULL;
+    return New_mesh;
 }
